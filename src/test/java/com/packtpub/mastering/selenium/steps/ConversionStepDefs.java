bd22232005a1092cc5194869df795e1b288379f3
optikool <optikool@yahoo.com>
2016-12-17 12:54:42 -0800

    Added pom.xml, added steps

---
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -0,0 +1,25 @@
+package com.packtpub.mastering.selenium.steps;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
+/**
+ * Created by optikool on 12/17/2016.
+ */
+public class ConversionStepDefs {
+    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
+    public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) {
+
+    }
+
+    @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
+    public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) {
+
+    }
+
+    @Then("^It should be converted to Celsius as (\\d+) degree$")
+    public void It_should_be_converted_to_Celsius_as_degree(int arg1) {
+
+    }
+}
